67731e673ce5ffb4b3d65ed696135dc1e94fd4f1
Anatoliy Kmetyuk <anatoliykmetyuk@yahoo.com>
2014-06-17 13:07:50 +0300

    adds extras to the classpath

--- buildvars.sh
+++ buildvars.sh
@@ -7,6 +7,7 @@ export   SUBSCRIPT_BIN="$SUBSCRIPT_BUILD/quick/bin"
 export     SCALA_REFLECT="$SUBSCRIPT_BUILD/quick/classes/reflect"
 export         SCALA_LIB="$SUBSCRIPT_BUILD/pack/lib/scala-library.jar"
 export SUBSCRIPT_CLASSES="$SUBSCRIPT_BUILD/quick/classes/subscript"
+export  SUBSCRIPT_EXTRAS="$SUBSCRIPT_BUILD/quick/classes/subscript-extras"
 export             SWING="$SUBSCRIPT_BUILD/pack/lib/scala-swing.jar"
 
 # SubScript compile-time dependencies
